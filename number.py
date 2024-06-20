018675af12973b07a8c674d77e9808318d7d55e0
XelgaAr <143228153+XelgaAr@users.noreply.github.com>
2024-06-20 15:23:42 +0300

    Dz_10.2

--- number.py
+++ number.py
@@ -1,45 +1,40 @@
 import logging
-import multiprocessing
-import threading
-import time
+from threading import Thread
 from multiprocessing import Process
 
-def action_function(num_start, num_end, name):
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-    logging.info(f"{name}: starting")
+logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO, datefmt="%H:%M:%S")
+
+
+def action_function(num_start, num_end, digits_len, name):
+    logging.info(f"Started {name}")
     count = 0
-    for i in range(int(num_start), int(num_end)):
-        string = str(i)
-        number_len = int(len(string) / 2)
-        first_part = string[0:number_len]
-        second_part = string[number_len + 1:len(string)]
-        first_sum = sum(int(i) for i in first_part)
-        second_sum = sum(int(i) for i in second_part)
-        if first_sum == second_sum:
+    number_len = digits_len // 2
+
+    for num in range(num_start, num_end):
+        string = str(num).zfill(digits_len)
+        left_str = string[:number_len]
+        right_str = string[number_len:]
+
+        left_sum = sum(int(j) for j in left_str)
+        right_sum = sum(int(j) for j in right_str)
+        if left_sum == right_sum:
             count += 1
-    logging.info(f"{name} finishing, start {int(num_start)}, end {int(num_end)}, count {count} ")
+
+    logging.info(f"Finishing {name}, start {num_start}, end {num_end}, count {count}")
 
 
 if __name__ == "__main__":
-    mim_number = 0
     max_number = 99999999
     divider = 4
-    num1 = max_number / divider
+    part = (max_number + 1) // divider
+    length = len(str(max_number))
 
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    for i in range(divider):
+        left_num = i * part
+        right_num = ((i + 1) * part) - 1
 
-    for i in range(1, divider + 1):
-        left_num = (i - 1) * num1
-        right_num = left_num + num1
-        x = threading.Thread(target=action_function, args=(left_num, right_num, f"Thread {i}"))
-        x.start()
+        t = Thread(target=action_function, args=(left_num, right_num, length, f"Thread {i + 1}"))
+        t.start()
 
-    for i in range(1, divider + 1):
-        left_num = (i - 1) * num1
-        right_num = left_num + num1
-        y = Process(target=action_function, args=(left_num, right_num, f"Process {i}"))
-        y.start()
-
-    logging.info("End of code")
+        p = Process(target=action_function, args=(left_num, right_num, length, f"Process {i + 1}"))
+        p.start()
